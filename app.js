31d02fae0cda1e35011232286ed99d5c10fa58a8
nade-harlow <omonadefranklyn@gmail.com>
2022-05-02 01:15:28 +0100

    html engin implemented

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express')
+const path = require('path')
+const app = new express()
+const ejs = require('ejs') 
+
+app.set('view engine','ejs')
 
 app.use(express.static('public'));
 
@@ -8,18 +11,22 @@ app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
-
 app.get('/', function(req, res){
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', function(req, res){
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/contact', function(req, res){
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
-app.get('/post',(req,res)=>{ res.sendFile(path.resolve(__dirname,'pages/post.html'))
+app.get('/post',(req,res)=>{ 
+    // res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 })
