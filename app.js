820cc78433b96b5e4f700b594ea8171f2164dee4
nade-harlow <omonadefranklyn@gmail.com>
2022-06-03 15:29:33 +0100

    post added to database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = new express()
 const ejs = require('ejs') 
 const mongoose = require('mongoose'); 
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/node', {useNewUrlParser: true});
 
@@ -21,17 +22,19 @@ const BlogPostSchema = new Schema({
 
 const BlogPost = mongoose.model('BlogPost',BlogPostSchema); 
 module.exports = BlogPost
-BlogPost.create({
-    title: 'The Mythbuster’s Guide to Saving Money on Energy Bills',
-    body: 'If you have been here a long time, you might remember when I went on ITV Tonight todispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics...'
-    }, (error, blogpost) =>{
-    console.log(error, blogpost) })
+// BlogPost.create({
+//     title: 'The Mythbuster’s Guide to Saving Money on Energy Bills',
+//     body: 'If you have been here a long time, you might remember when I went on ITV Tonight todispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics...'
+//     }, (error, blogpost) =>{
+//     console.log(error, blogpost) })
     
 
 
 app.set('view engine','ejs')
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
@@ -55,4 +58,13 @@ app.get('/contact', function(req, res){
 app.get('/post',(req,res)=>{ 
     // res.sendFile(path.resolve(__dirname,'pages/post.html'))
     res.render('post')
+})
+
+app.get('/post/new', (req,res)=>{ 
+   res.render('create')
+ })
+
+app.post('/post/store', async(req,res)=>{
+   await BlogPost.create(req.body)
+    res.redirect('/')
 })
